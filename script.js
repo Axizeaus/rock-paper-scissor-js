bc9d2646c16b41925c026ffe4beecae213ec8e48
Axizeaus <axizeaus@gmail.com>
2022-03-03 04:49:15 +0630

    modify get_user_input()

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// function get_user_input(){
-//     let choice = prompt("rock, paper or scissor?")
-//     console.log(`You choose : ${choice.toLowerCase()}`)
-//     return choice.toLowerCase()
-// }
-
-// function computer_play() {
-//     computer_choice = Math.floor(Math.random() * 3);
-//     if (computer_choice == 0){
-//         console.log(`computer choose rock`)
-//         return "rock";
-//     }
-//     if (computer_choice == 1){
-//         console.log(`computer choose paper`)
-//         return "paper";
-//     }
-//     if (computer_choice == 2){
-//         console.log(`computer choose scissor`)
-//         return "scissor";
-//     }
-// }
+function computer_play() {
+    // input : none 
+    // return type : str 
+    // return : rock OR paper OR scissor 
+    computer_choice = Math.floor(Math.random() * 3);
+    if (computer_choice == 0) {
+        console.log(`computer choose rock`)
+        return "rock";
+    }
+    if (computer_choice == 1) {
+        console.log(`computer choose paper`)
+        return "paper";
+    }
+    if (computer_choice == 2) {
+        console.log(`computer choose scissor`)
+        return "scissor";
+    }
+}
 
 // function choose_rock(player_choice, computer_choice){
 //     if (player_choice == 'rock' && computer_choice == 'paper'){
@@ -37,7 +34,7 @@
 //         return player_choice;
 //     }
 // }
-    
+
 // function choose_scissor(player_choice, computer_choice){
 //     if (player_choice == 'scissor' && computer_choice == 'rock'){
 //         return computer_choice;
@@ -60,15 +57,7 @@
 //     }
 // }
 
-// function play_a_single_round(){
-//     let player_choice = get_user_input();
-//     let computer_choice = computer_play();
-//     if (player_choice == computer_choice){
-//         console.log("it is a draw");
-//     } else {
-//         game_mechanism(player_choice, computer_choice);
-//     }
-// }
+
 
 // // function play_multiple_rounds(){
 // //     let num_of_times = prompt("How many times would you like to play?");
@@ -79,20 +68,50 @@
 
 // play_a_single_round()
 
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', (e) =>{
-    console.log("this is rock working");
-    console.log(e)
-})
 
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', (e) => {
-    console.log('this is paper working');
-    console.log(e)
-});
+function get_user_input() {
 
-const scissor = document.querySelector('#scissor');
-scissor.addEventListener('click', (e) => {
-    console.log('this is scissor working');
-    console.log(e)
-})
+    // let player_choice = 'nothing';
+
+    const rock = document.querySelector('#rock');
+    rock.addEventListener('click', () => {
+        let player_choice = 'rock';
+        return player_choice;
+    });
+
+    const paper = document.querySelector('#paper');
+    paper.addEventListener('click', () => {
+        let player_choice = 'paper';
+        return player_choice;
+    });
+
+    const scissor = document.querySelector('#scissor');
+    scissor.addEventListener('click', () => {
+        let player_choice = 'scissor';
+        return player_choice;
+    });
+    
+}
+
+// function play_a_single_round() {
+//     let player_choice = get_user_input();
+//     let computer_choice = computer_play();
+//     if (player_choice == computer_choice) {
+//         console.log("it is a draw");
+//     } else {
+//         game_mechanism(player_choice, computer_choice);
+//     }
+// }
+
+function display_on_screen() {
+    const container = document.querySelector('#scoreboard');
+
+    const content = document.createElement('div');
+    content.classList.add('content')
+    content.textContent = get_user_input();
+
+    container.appendChild(content);
+}
+
+display_on_screen()
+// console.log(get_user_input())
